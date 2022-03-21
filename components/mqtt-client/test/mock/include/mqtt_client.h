48df11c733a713570ae413251f920bb96db1b489
Borys Chyliński <borchy97@gmail.com>
2022-03-21 20:02:57 +0100

    [license] Add license.

--- components/mqtt-client/test/mock/include/mqtt_client.h
+++ components/mqtt-client/test/mock/include/mqtt_client.h
@@ -1,4 +1,22 @@
+// Copyright 2022 Borys Chyliński
 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
 #include "esp_err.h"
 
@@ -113,8 +131,8 @@ inline int esp_mqtt_client_publish(esp_mqtt_client_handle_t client,
     return 23;
 }
 
-inline int esp_mqtt_client_subscribe(esp_mqtt_client_handle_t client,
-    const char* topic, int qos) noexcept
+inline int esp_mqtt_client_subscribe(
+    esp_mqtt_client_handle_t client, const char* topic, int qos) noexcept
 {
     client->dispatch_fut =
         std::async(std::launch::async, [client, topic{ std::string(topic) }]() {
@@ -124,8 +142,8 @@ inline int esp_mqtt_client_subscribe(esp_mqtt_client_handle_t client,
     return 23;
 }
 
-inline int esp_mqtt_client_unsubscribe(esp_mqtt_client_handle_t client,
-    const char* topic) noexcept
+inline int esp_mqtt_client_unsubscribe(
+    esp_mqtt_client_handle_t client, const char* topic) noexcept
 {
     client->dispatch_fut = std::async(std::launch::async,
         [client]() { client->dispatch(MQTT_EVENT_UNSUBSCRIBED); });
